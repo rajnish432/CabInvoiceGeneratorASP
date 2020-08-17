d4acc6d9481e8ba4f61deb5634cdee2b35eedfba
rajnish432 <rajnish.kahar1996@gmail.com>
2020-08-17 15:06:56 +0530

    [Rajnish]ADDED: Welcome To Cab Invoice Generator

---
+++ CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class CabInvoiceGeneratorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
